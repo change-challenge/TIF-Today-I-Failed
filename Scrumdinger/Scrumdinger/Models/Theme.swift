5db1a58473a7f84b39057be5867ae56806a22e8a
change-challenge <dog941128@hotmail.com>
2023-03-22 16:18:39 +0900

    [Feat] 기본 구조 완성 ScrumView -> DetailView -> MeetingView

--- Scrumdinger/Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -29,12 +29,15 @@ enum Theme: String, Codable {
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
         }
     }
     
     var mainColor: Color {
         Color(rawValue)
+    }
+    var name: String {
+        rawValue.capitalized
     }
 }
